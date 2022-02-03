463f61f7cbb1cb8edab419c599c798f5e297ddd5
darnelld <darnelldugger@gmail.com>
2022-02-03 08:47:17 -0500

    finish dom to do lab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('I am working')
+const inputElement = document.getElementById('inMain')
+
+const btnElement = document.getElementById('submit-button')
+
+const ulElement = document.getElementById('todo-list')
+
+btnElement.addEventListener('click', function(evt) {
+  const li = document.createElement('li')
+  const inp = document.querySelector('input')
+  li.textContent = inp.value
+  if (inp.value !== ""){
+
+    document.querySelector('ul').appendChild(li)
+   inp.value = ""
+     console.dir(inp.value)
+  }
+})
